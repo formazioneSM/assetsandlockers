c089c44727253c2089b44e5ffeaef8cef0099e61
Ilaria <idellaquila@sysmanagement.it>
2022-07-22 15:59:12 +0200

    Aggiunta pagina scelta piano sede napoli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FloorComponent } from './features/floor/floor.component';
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 
@@ -13,6 +14,11 @@ const routes: Routes = [
     path: 'home',
 
     component: HomeComponent
+  },
+  {
+    path: 'floor',
+
+    component: FloorComponent
   },
   { path: '**', redirectTo: '/home' },];
 
